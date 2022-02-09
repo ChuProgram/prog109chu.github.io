0e85daadeef305ab89bd00d02509d080812b41c5
prog109chu <97070822+prog109chu@users.noreply.github.com>
2022-02-08 21:29:31 -0800

    Update validator.js

--- artifact04/validator.js
+++ artifact04/validator.js
@@ -166,13 +166,13 @@ function password(){
   var validPassword=false;
   var errorMessages = "";
   
-  var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{4,7}$/;
+  var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{1,7}$/;
 
   //2) read value from HTML
   var userPassword = document.getElementById("Password").value;
 
   //3) Do validation
-  if (!pattern.test(userPassword)) {
+  if (userPassword.length > 7 || !pattern.test(userPassword)) {
     errorMessages = "<p>Password is required and cannot be greater than 7 characters. Password requires 1 upper-case, 1 lower-case, 1 numeric, 1 special character</p>";
     console.log("Invalid Password")
   }
